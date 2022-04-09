ab3f32c32e856a8ca68d6a07429e89b4b1b43f39
TinsaeAregahagn <tinsaearegahagntm@gmail.com>
2022-04-09 01:38:09 -0700

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  *
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,4 +14,3 @@ printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
-
