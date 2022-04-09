fa17ab69299fbdbc3cd4e8dac08700ba9c6d1a92
TinsaeAregahagn <tinsaearegahagntm@gmail.com>
2022-04-09 00:57:35 -0700

    L

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf9"size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf9"Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
