efac477161deea0d39286a9bb79b2bb3f7e313c5
TinsaeAregahagn <tinsaearegahagntm@gmail.com>
2022-04-27 18:17:57 +0300

    l

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _puts_recursion - prints a string
- * @s: the pointer input
- *
+ * _puts_recursion - prints a string, followed by a new line.
+ * @a: string to be printed
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(++s);
+
+	else
+		_putchar('\n');
 }
